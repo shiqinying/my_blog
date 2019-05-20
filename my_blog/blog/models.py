f82f4ea9ff524905cfd2cb40bb8a3ecd0640d569
shiqinying <690037596@qq.com>
2019-05-20 09:22:39 +0800

    增加Post.image字段作为文章封面

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -77,6 +77,7 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=255, verbose_name="标题")
+    image = models.ImageField(verbose_name='封面图片', default='http://placehold.it/750x300')
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为MarkDown格式")
     content_html = models.TextField(verbose_name='正文html代码', blank=True, null=True, editable=False)
